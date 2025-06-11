63c30d17e80a228c098c63c8416cee1fe9760285
Ranojit <ranojitdas362@gmail.com>
2025-06-12 00:43:59 +0530

    commit added

--- src/Strings_builder_buffer.kt
+++ src/Strings_builder_buffer.kt
@@ -21,23 +21,26 @@ fun main(){
     // Useful Methods
     println(name.length)
     println(name[0])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    println(name.substring(0,5))
+    println(name.contains("Coding"))
+    println(name.replace("Coding","Ranojit"))
+    println(name.uppercase())
+    println(name.isEmpty())
+    println(name.isNullOrBlank())
     println(name)
     println(multiLine)
     println(escapedString)
+
+
+    // StringBuilder , StringBuffer
+
+    val sb = StringBuilder("Hello")
+    sb.append(" World"," Hi Kotlin")
+    sb.insert(0,"hi ")
+    println(sb)
+
+    val sbuffer = StringBuffer("Hello")
+    sbuffer.append(" World"," Hi Kotlin")
+    sbuffer.insert(0,"hi ")
+    println(sbuffer)
 }

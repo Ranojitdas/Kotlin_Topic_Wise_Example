0975e8eececc54008d5c0fd16a05e32cc012214a
Ranojit <ranojitdas362@gmail.com>
2025-06-12 12:59:20 +0530

    Console_input_output.kt added

---
+++ src/Console_input_output.kt
@@ -0,0 +1,11 @@
+
+fun main(){
+
+    println("Enter your name :")
+    val name = readLine()
+    println("Hello $name")
+
+    println("Enter your name :")
+    val name2 = readln()     // after 1.6.0
+    println("Hello $name2")
+}
